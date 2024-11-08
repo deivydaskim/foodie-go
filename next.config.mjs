edff5a59442fb26b214b6b66cf505350312787ae
deivydas.kim <deivydas.kim@gmail.com>
2024-11-08 13:08:06 +0200

    add svgr/webpack and headlesui deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   experimental: {
     reactCompiler: true, // Enable the React Compiler for React 19
   },
